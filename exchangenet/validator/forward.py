d07e2a6e0a445e778370a633d13ea554fca6bd29
taomind007 <likebitmania007@gmail.com>
2024-03-26 12:41:31 -0500

    feat: forward a query based on SwapId

--- exchangenet/validator/forward.py
+++ exchangenet/validator/forward.py
@@ -20,13 +20,12 @@
 import bittensor as bt
 import time
 
-from exchangenet.protocol import SwapNotification, FinalizeSwap
+from exchangenet.protocol import SwapRequest, SwapNotification
 from exchangenet.validator.reward import get_rewards
 from exchangenet.utils.uids import get_random_uids
 from exchangenet.utils.swap import create_swap
 
-
-async def forward(self):
+async def forward(self, query: SwapRequest):
     """
     The forward function is called by the validator every time step.
 
@@ -47,22 +46,26 @@ async def forward(self):
     responses = await self.dendrite(
         # Send the query to selected miner axons in the network.
         axons=[self.metagraph.axons[uid] for uid in miner_uids],
-        # Construct a dummy query. This simply contains a single integer.
-        synapse=FinalizeSwap(dummy_input=self.step),
+        # Construct a query based on swapId.
+        synapse=SwapNotification(swap_id=query.swap_id),
         response_time = self.dendrite.response_time,
         # All responses have the deserialize function called on them before returning.
         # You are encouraged to define your own deserialization function.
         deserialize=True,
     )
 
+    # Wait until the swap is expired
+    time.sleep(self.expiry_time)
+
     # Log the results for monitoring purposes.
     bt.logging.info(f"Received responses: {responses}")
 
     # TODO(developer): Define how the validator scores responses.
     # Adjust the scores based on responses from miners.
-    rewards = get_rewards(self, query=self.step, responses=responses)
+    rewards = get_rewards(self, query=query, responses=responses)
 
     bt.logging.info(f"Scored responses: {rewards}")
+    
     # Update the scores based on the rewards. You may want to define your own update_scores function for custom behavior.
     self.update_scores(rewards, miner_uids)
 

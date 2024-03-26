905eeab981e7ebc8ccb24dcf5767d4fc0f8a8055
taomind007 <likebitmania007@gmail.com>
2024-03-26 15:19:54 -0500

    chore: move winner_reward_rate to config.py

--- exchangenet/utils/config.py
+++ exchangenet/utils/config.py
@@ -234,6 +234,13 @@ def add_validator_args(cls, parser):
         help="The name of the project where you are sending the new run.",
         default="opentensor-dev",
     )
+    
+    parser.add_argument(
+        "--neuron.winner_reward_rate",
+        type=float,
+        help="The rate at which the winner is rewarded.",
+        default=2.0,
+    )
 
 
 def config(cls):
